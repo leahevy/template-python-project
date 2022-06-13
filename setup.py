eb9e1b601cee704600de9abe9b99d116d6e678aa
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-13 23:36:41 +0200

    Bump version: 0.0.1 → 0.1.0
    Signed-off-by: Leah Lackner <leah.lackner+github@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as f:
 
 setup_info = dict(
     name="template-python-with-setup",
-    version="0.0.1",
+    version="0.1.0",
     author="Leah Lackner",
     author_email="leah.lackner+github@gmail.com",
     url="https://github.com/leahevy/template-python-with-setup",
