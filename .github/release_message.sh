77a2e150f6f6e74b1b3285b223b76550b32ff8cf
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-16 17:59:32 +0200

    Improve release workflow message (2)

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -2,17 +2,22 @@
 current_tag="$(git tag --sort=-creatordate | head -n 1)"
 previous_tag="$(git tag --sort=-creatordate | sed -n 2p)"
 
+
 echo "## Version ${current_tag}"
+echo
+echo "### Project links"
 echo
 echo "- Source code: <https://github.com/leahevy/template-python-project/tree/$current_tag>"
 echo "- Release: <https://github.com/leahevy/template-python-project/releases/tag/$current_tag>"
+echo "- Download: <https://github.com/leahevy/template-python-project/archive/refs/tags/$current_tag.zip>"
 echo
 echo "### Changelog"
 echo
-git shortlog -e -n "${previous_tag}.." |
+git shortlog -n "${previous_tag}.." |
     sed 's/^./    &/' |
-    sed 's/ ([[:digit:]][[:digit:]]*):/:/g' |
+    sed 's/ ([[:digit:]][[:digit:]]*)://g' |
     sed '/\s*Release: version /d' |
     sed '/\s*Bump version: /d' |
     sed 's/^    //g' |
+    sed 's/^\([^ ]\)/#### \1/g' |
     sed 's/^  */- /g'
