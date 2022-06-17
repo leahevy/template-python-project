26591d7a3cfa3f4f7b266ce3e43d868bac27c084
Leah Lackner <leah.lackner+github@gmail.com>
2022-06-17 08:54:58 +0200

    Inject package with fixture + license headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,36 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2022 Leah Lackner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 from typing import Any, Generator
 
 import pytest
 
-import template_python_project
+import template_python_project as _template_python_project
 
 
 @pytest.fixture(autouse=True)
 def add_imports(doctest_namespace: dict[str, Any]) -> None:
-    doctest_namespace["tp"] = template_python_project
+    doctest_namespace["tp"] = _template_python_project
+
+
+@pytest.fixture()
+def template_python_project() -> Any:
+    return _template_python_project
 
 
 @pytest.fixture(autouse=True)
--- tests/test_template_python_project_main.py
+++ tests/test_template_python_project_main.py
@@ -1,9 +1,30 @@
-from template_python_project import api
+#!/usr/bin/env python
+#
+# Copyright (C) 2022 Leah Lackner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from types import ModuleType
 
 
-def test_template_python_project_api_some() -> None:
-    api.some_api_func()
+def test_template_python_project_api_some(
+    template_python_project: ModuleType,
+) -> None:
+    template_python_project.api.some_api_func()
 
 
-def test_template_python_project_api_other() -> None:
-    api.other_api_func()
+def test_template_python_project_api_other(
+    template_python_project: ModuleType,
+) -> None:
+    template_python_project.api.other_api_func()
